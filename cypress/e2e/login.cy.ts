decd3a169fff71a7867082959d15980cc35a2cf1
thanhson <thanhson9112@gmail.com>
2023-11-08 21:31:17 +0700

    Update

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,6 +1,6 @@
 describe('Login Page', () => {
   it('Login', () => {
-    cy.visit('/login')
+    cy.visit('https://zuhot-cinema.vercel.app/login')
     cy.get('input[type="email"]').type("doan@gmail.com")
     cy.get('input[type="password"]').type("12345678")
     cy.get('button').click()
