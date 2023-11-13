7f8c2602bcb271c0a4244c53d5d94022a76b544a
khangho23 <hohoangkhang18@gmail.com>
2023-11-14 00:26:25 +0700

    c

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -3,24 +3,32 @@ describe('Search home', () => {
     cy.visit('');
     cy.get('input[type="text"]').type('Yêu');
     cy.get('.ant-btn').click();
+    cy.get('#\\30  > .ant-card > .ant-card-body > :nth-child(1) > .text-center > .text-sm')
+        .should('include.text', 'YÊU');
   })
 
   it('Search by country', () => {
     cy.visit('');
     cy.get('#country').select('THÁI LAN');
     cy.get('.mt-6 > .w-full').click();
+    cy.get('#\\30 > .ant-card > .ant-card-body > :nth-child(1) > .text-center > .text-sm')
+        .should('include.text', 'ÁC QUỶ MA SƠ II');
   })
 
   it('Search by type of movie', () => {
     cy.visit('');
     cy.get('#movieType').select('TÌNH CẢM');
     cy.get('.mt-6 > .w-full').click();
+    cy.get('#\\30 > .ant-card > .ant-card-body > :nth-child(1) > .text-center > .text-sm')
+          .should('include.text', 'CHẠM VÀO HẠNH PHÚC');
   })
 
   it('Search by branch', () => {
     cy.visit('');
     cy.get('#branch').select('TÂN PHÚ');
     cy.get('.mt-6 > .w-full').click();
+    cy.get('#\\30 > .ant-card > .ant-card-body > :nth-child(1) > .text-center > .text-sm')
+        .should('include.text', 'ÁC QUỶ MA SƠ II');
   })
 
   it('Movie is showing', () => {
@@ -29,6 +37,8 @@ describe('Search home', () => {
     cy.getCookie('statusId').then((cookie) => {
       expect(cookie?.value).to.equal('0');
     });
+    cy.get('#\\30 > .ant-card > .ant-card-body > :nth-child(1) > .text-center > .text-sm')
+        .should('include.text', 'HẠ CÁNH KHẨN CẤP');
   })
 
   it('Upcomming movie', () => {
@@ -37,5 +47,7 @@ describe('Search home', () => {
     cy.getCookie('statusId').then((cookie) => {
       expect(cookie?.value).to.equal('1');
     });
+    cy.get('#\\30 > .ant-card > .ant-card-body > :nth-child(1) > .text-center > .text-sm')
+        .should('include.text', 'ĐỊA ĐÀNG SỤP ĐỔ');
   })
 })
