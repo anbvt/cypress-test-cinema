7fc8e638a8be51bc21b200074d5f41ac5beebe82
dkminh.hcm <dkminh.hcm@vnpt.vn>
2023-11-15 21:52:31 +0700

    change

--- cypress.config.js
+++ cypress.config.js
@@ -7,13 +7,6 @@ export default defineConfig({
     },
     baseUrl: "http://localhost:3000",
   },
-
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
   viewportWidth: 1440,
   viewportHeight: 900,
 });
